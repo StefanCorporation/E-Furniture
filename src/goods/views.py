c7abf0c1b08bec3a9a3df4aa91619e413c75f495
Stefan Ashugyan <abrams1234518@gmail.com>
2025-01-14 18:00:42 +0400

    Create filtration by products

--- src/goods/views.py
+++ src/goods/views.py
@@ -9,6 +9,10 @@ from goods.models import Products
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
+
 
     if category_slug == 'all':
         goods = Products.objects.all()
@@ -16,6 +20,14 @@ def catalog(request, category_slug):
         # Через filter() мы филтруем по категории, обращаясь к полю category
         # ЧЕРЕЗ САМ ТОВАР с помощю ForeignKey()
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = Products.objects.filter(discount__gt=0) # если скидка выше 0
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by) # через метод order_by возвращаем сортировку либо от 
+                                                    # меньшего к большому либо наоборот
+
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page)) # отабражает нужную нам страницу
