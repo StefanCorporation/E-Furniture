ffa6f8519a7f2ae3cc28316b2fe51a05b6da5452
Stefan Ashugyan <abrams1234518@gmail.com>
2025-01-22 20:59:33 +0400

    Configurate full text search by django base functionality

--- src/goods/utils.py
+++ src/goods/utils.py
@@ -1,4 +1,6 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
+
 from goods.models import Products
 
 
@@ -8,14 +10,48 @@ def q_search(query):
         return Products.objects.filter(id=int(query))
     
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description", config='russian')
+    query = SearchQuery(query, config='russian')
 
-    q_objects = Q()
+    result = ( 
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel="<span style='background-color: yellow';>",
+            stop_sel="</span>",
+        )
+    )
 
 
-    for token in keywords:
-        q_objects |= Q(name__icontains=token)
-        q_objects |= Q(description__icontains=token)
+    result = result.annotate( 
+        bodyline = SearchHeadline(
+            "description",
+            query,
+            start_sel="<span style='background-color: yellow';>",
+            stop_sel="</span>",
+        )
+    )
 
-    return Products.objects.filter(q_objects)
+    return result
+
+
+
+
+    # Простой поиск по имени и описанию
+    # keywords = [word for word in query.split() if len(word) > 2]
+
+    # q_objects = Q()
+
+
+    # for token in keywords:
+    #     q_objects |= Q(name__icontains=token)
+    #     q_objects |= Q(description__icontains=token)
+
+    # return Products.objects.filter(q_objects)
     
