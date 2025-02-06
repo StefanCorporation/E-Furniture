56fddd95abbcc5e6bc17237c1eb559b3ba4bc73a
Stefan Ashugyan <abrams1234518@gmail.com>
2025-02-06 14:12:33 +0400

    Optinized forms in templates

--- src/users/forms.py
+++ src/users/forms.py
@@ -4,5 +4,11 @@ from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
+
     class Meta:
         model = User
+        fields = ['username', 'password']
+
+        
