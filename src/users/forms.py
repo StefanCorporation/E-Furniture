eb1531dcb2f6301355ea0e64ea9be3b6e3480863
Stefan Ashugyan <abrams1234518@gmail.com>
2025-02-07 13:29:05 +0400

    Add errors notification functionality

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,11 +1,8 @@
-from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField()
-    password = forms.CharField()
 
     class Meta:
         model = User
