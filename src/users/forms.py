bacdcf1d36586c5bb3e35b047ed42e7eb465114b
Stefan Ashugyan <abrams1234518@gmail.com>
2025-02-09 15:22:51 +0400

    Add registration functionallity, configurate templates

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
+
 
 class UserLoginForm(AuthenticationForm):
 
@@ -9,3 +11,22 @@ class UserLoginForm(AuthenticationForm):
         fields = ['username', 'password']
 
         
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = [
+            'firstname',
+            'lastname',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        ]
+
+    firstname = forms.CharField()
+    lastname = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
