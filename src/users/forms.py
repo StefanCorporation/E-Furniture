8099e6823c648db8dc0a83a690b7281d803a9341
Stefan Ashugyan <abrams1234518@gmail.com>
2025-02-10 18:33:51 +0400

    Add profile page and configurate profile data update functionality

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -16,17 +16,37 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = [
-            'firstname',
-            'lastname',
+            'first_name',
+            'last_name',
             'username',
             'email',
             'password1',
             'password2'
         ]
 
-    firstname = forms.CharField()
-    lastname = forms.CharField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
     username = forms.CharField()
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+
+
+class ProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = [
+            'images',
+            'first_name',
+            'last_name',
+            'username',
+            'email'
+        ]
+
+    images = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
--- src/users/views.py
+++ src/users/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 # Create your views here.
 
@@ -57,16 +58,29 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
-
+@login_required
 def profile(request):
 
+    if request.method == 'POST':
+       
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+
+        if form.is_valid():                      
+            form.save()
+            
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
+
     context = {
-        'title': 'Home - Profile'
+        'title': 'Profile',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
 
 
-
+@login_required
 def logout(request):
     auth.logout(request)
     return redirect(reverse('main:index'))
